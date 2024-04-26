e6ccac1a92c5b8079892396e1dbfd82dc70cc60f
WolfEclipses <dragonwolfgaming97@gmail.com>
2024-04-26 00:21:36 -0400

    first commit

---
+++ src/app/page.tsx
@@ -0,0 +1,6 @@
+import Button from "@/components/UI/Button";
+
+export default async function Home() {
+
+  return <Button variant='ghost'>Hello</Button>
+}
---
+++ src/types/db.d.ts
@@ -0,0 +1,27 @@
+interface User {
+    name: string
+    email: string
+    image: string
+    id: string
+}
+
+interface Chat {
+    id: string
+    messages: Message[]
+}
+
+interface Message {
+    id: string
+    senderId: string
+    recieverId: string
+    text: string
+    timestamp: number
+
+}
+
+interface FriendRequest {
+    id: string
+    senderId: string
+    recieverId: string
+    
+}
