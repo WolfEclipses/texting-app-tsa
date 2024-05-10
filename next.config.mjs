790ea02ccbe93850e2db6ef8b7fefb0f88b71870
WolfEclipses <dragonwolfgaming97@gmail.com>
2024-05-10 19:21:39 -0400

    TestCommit

--- next.config.mjs
+++ next.config.mjs
@@ -2,6 +2,7 @@
 const nextConfig = {
     experimental: {
         appDir: true,
+        NEXTAUTH_URL: process.env.NEXTAUTH_URL,
     },
     images: {
         domains: ['lh3.googleusercontent.com']
--- src/app/(dashboard)/dashboard/layout.tsx
+++ src/app/(dashboard)/dashboard/layout.tsx
@@ -41,7 +41,7 @@ const Layout = async ({ children }: LayoutProps) => {
     return (<div className = 'w-full flex h-screen'>
         <div className='flex h-full w-full max-w-xs grow flex-col gap-y-5 overflow-y-auto border-r border-gray-200 bg-white px-6'>
             <Link href='/dashboard' className='flex h-16 shrink-0 items-center'>
-                <Icons.Logo className='h-8 w-auto text-indigo-600' />
+                <Icons.Logo className='h-20 w-auto text-indigo-600 flex ' />
             </Link>
             
             {friends.length > 0 ? (
